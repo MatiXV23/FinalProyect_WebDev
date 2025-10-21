530654d4964cc582cc31d020b7f2a879dbaa5525
brahnu <nunezbrahian@gmail.com>
2025-10-20 21:20:18 -0300

    ruta obtener articulo por categoria

--- api/src/routes/articulo/_id_categoria/id_categoria.ts
+++ api/src/routes/articulo/_id_categoria/id_categoria.ts
@@ -1,2 +1,20 @@
-//get (params, categoría)
+import { FastifyPluginAsyncTypebox, Type } from "@fastify/type-provider-typebox";
+import { articuloModel } from "../../../models/articuloModel";
+import { PC_NotImplemented } from "../../../errors/errors";
 
+
+const articuloByCategoriaRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
+        fastify.get("/",{
+        schema:{
+            summary:"Obtener articulo",
+            tags: ["Articulo"],
+            description:"Ruta para obtener articulos por categoria",
+            params: Type.Pick(articuloModel,["categoria"]),
+            response: {
+                200: Type.Array(articuloModel)
+            }
+        }
+    },async (req, rep)=>{
+        return new PC_NotImplemented()
+    });
+}
