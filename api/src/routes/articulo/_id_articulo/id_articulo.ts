8d544e6c64a0aabdb8fbccf25932a281280e8114
brahnu <nunezbrahian@gmail.com>
2025-10-20 21:04:40 -0300

    ruta id_articulo

--- api/src/routes/articulo/_id_articulo/id_articulo.ts
+++ api/src/routes/articulo/_id_articulo/id_articulo.ts
@@ -1,1 +1,36 @@
-//patch - delete - get
+import { FastifyPluginAsyncTypebox, Type } from "@fastify/type-provider-typebox"
+import {articuloModel} from "../../../models/articuloModel"
+import { PC_NotImplemented } from "../../../errors/errors"
+
+
+//necesito autorizacion, solo el admin puede moficar el artículo
+const articuloByIdRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
+    fastify.put("/", {
+        schema:{
+            summary:"Modificar articulo",
+            tags: ["Articulo"],
+            description: "Ruta para modificar articulo",
+            body: Type.Partial(articuloModel),
+            params: Type.Pick(articuloModel,["id_articulo"]),
+            response: {
+                201: articuloModel,
+            }
+        }
+    }, async (req, rep)=>{
+        return new PC_NotImplemented()
+    });
+
+    fastify.delete("/", {
+        schema:{
+            summary: "Eliminar articulo",
+            tags: ["Articulo"],
+            description:"Ruta para eliminar articulo",
+            params: Type.Pick(articuloModel, ["id_articulo"]),
+            response: {
+                201: Type.Null()
+            }
+        }
+    }, async (req, rep)=>{
+        return new PC_NotImplemented()
+    })
+}
