94b610143a3720657720033200a459687655b746
brahnu <nunezbrahian@gmail.com>
2025-10-31 15:43:01 -0300

    Rutas usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './routes/home/home.page';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage, title: 'Home' },
+  {
+    path: 'usuarios',
+    loadComponent: async () =>
+      (await import('./routes/usuarios/pages/usuarios/usuarios.page')).UsuariosPage,
+    title: 'Usuarios',
+  },
+
+  {
+    path: 'usuarios/edit',
+    loadComponent: async () =>
+      (await import('./routes/usuarios/pages/usuarios-edit/usuarios-edit.page')).UsuariosEditPage,
+    title: 'Editar usuarios',
+  },
+
+  {
+    path: 'usuarios/login',
+    loadComponent: async () =>
+      (await import('./routes/usuarios/pages/usuarios-login/usuarios-login.page'))
+        .UsuariosLoginPage,
+    title: 'Logear usuario',
+  },
+
+  {
+    path: 'usuarios/register',
+    loadComponent: async () =>
+      (await import('./routes/usuarios/pages/usuarios-register/usuarios-register.page'))
+        .UsuariosRegisterPage,
+    title: 'Regsitrar usuario',
+  },
+];
