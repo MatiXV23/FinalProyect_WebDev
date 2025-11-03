b575c32c4a8f98892fceaa7fa2d3c4025e4c4dee
brahnu <nunezbrahian@gmail.com>
2025-11-03 15:32:28 -0300

    get ruta categoria

--- api/src/routes/categorias/categorias.ts
+++ api/src/routes/categorias/categorias.ts
@@ -2,8 +2,13 @@ import {
   type FastifyPluginAsyncTypebox,
   Type,
 } from "@fastify/type-provider-typebox";
-import { PC_NotImplemented } from "../../errors/errors.ts";
+import {
+  PC_Error,
+  PC_NotFound,
+  PC_NotImplemented,
+} from "../../errors/errors.ts";
 import { categoriaModel } from "../../models/market/categoriaModel.ts";
+import { myPool } from "../../services/data/db_service.ts";
 
 const categoriasRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.get(
@@ -20,32 +25,41 @@ const categoriasRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async (req, rep) => {
-      throw new PC_NotImplemented();
-      return;
+      return await fastify.CategoriasDB.getAll();
     }
   );
 
-  fastify.post(
-    "",
-    {
-      schema: {
-        summary: "Crear categoria",
-        tags: ["Categoria"],
-        description:
-          "Ruta para crear categoria. Se requiere ser un administrador",
-        body: Type.Omit(categoriaModel, ["id_categoria"]),
-        response: {
-          201: categoriaModel,
-        },
-        security: [{ bearerAuth: [] }],
-      },
-      onRequest: [fastify.authenticate, fastify.isAdmin],
-    },
-    async (req, rep) => {
-      throw new PC_NotImplemented();
-      return;
-    }
-  );
+  // fastify.post(
+  //   "",
+  //   {
+  //     schema: {
+  //       summary: "Crear categoria",
+  //       tags: ["Categoria"],
+  //       description:
+  //         "Ruta para crear categoria. Se requiere ser un administrador",
+  //       body: Type.Omit(categoriaModel, ["id_categoria"]),
+  //       response: {
+  //         201: categoriaModel,
+  //       },
+  //       // security: [{ bearerAuth: [] }],
+  //     },
+  //     // onRequest: [fastify.authenticate, fastify.isAdmin],
+  //   },
+  //   async (req, rep) => {
+  //     try {
+  //       const { nombre } = req.body;
+  //       const resultado = await myPool.query(
+  //         `INSERT INTO categorias (nombre) VALUES ($1) RETURNING id_categoria, nombre`,
+  //         [nombre]
+  //       );
+  //       return rep.status(201).send("Usuario creado correctamente");
+  //     } catch (error) {
+  //       return rep.status(500).send({
+  //         PC_Error,
+  //       });
+  //     }
+  //   }
+  // );
 };
 
 export default categoriasRoutes;
--- api/src/services/data/categoriesDB.ts
+++ api/src/services/data/categoriesDB.ts
@@ -1,39 +1,47 @@
 import { BasePgRepository } from "../../models/common/baseRepository.ts";
-import { PC_NotFound, PC_BadRequest, PC_InternalServerError, PC_NotImplemented } from "../../errors/errors.ts";
+import {
+  PC_NotFound,
+  PC_BadRequest,
+  PC_InternalServerError,
+  PC_NotImplemented,
+} from "../../errors/errors.ts";
 import type { Pool } from "pg";
 import type { Categoria } from "../../models/market/categoriaModel.ts";
-
+import { myPool } from "./db_service.ts";
 
 // TODO: REALIZAR ESTO ! ! !
 export class CategoriasDB extends BasePgRepository<Categoria> {
+  static rows:
+    | { id_categoria: number; nombre: string }[]
+    | PromiseLike<{ id_categoria: number; nombre: string }[]>;
+  constructor(pool: Pool) {
+    super(pool);
+  }
 
-    constructor(pool: Pool) {
-        super(pool)
-    }
-
-    #baseQuery = /*sql*/`
+  #baseQuery = /*sql*/ `
                 SELECT 
                     u.*
                 FROM categorias u
-                `
+                `;
 
-    async getAll(): Promise<Categoria[]> {
-        throw new PC_NotImplemented()
-    }
-    
-    async getById(id:number): Promise<Categoria> {
-        throw new PC_NotImplemented()
-    }
+  async getAll(): Promise<Categoria[]> {
+    const resultado = await this.pool.query<Categoria>(this.#baseQuery);
+    return resultado.rows;
+  }
 
-    async create(data: Partial<Categoria>): Promise<Categoria> {
-        throw new PC_NotImplemented()
-    }
+  async getById(id: number): Promise<Categoria> {
+    throw new PC_NotImplemented();
+  }
 
-    async update(id: number, data: Partial<Categoria>): Promise<Categoria> {
-        throw new PC_NotImplemented()
-    }
+  async create(data: Partial<Categoria>): Promise<Categoria> {
+    throw new PC_NotImplemented();
+  }
 
-    async delete(id: number): Promise<void> {
-        throw new PC_NotImplemented()
-    }
+  async update(id: number, data: Partial<Categoria>): Promise<Categoria> {
+    throw new PC_NotImplemented();
+  }
+
+  async delete(id: number): Promise<void> {
+    throw new PC_NotImplemented();
+  }
 }
