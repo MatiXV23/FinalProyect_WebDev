f2dddc61383df9f7a03b0f3c353df5df333c843b
Agustin <agustincigaran@icloud.com>
2025-11-06 18:21:27 -0300

    base form

--- frontend/src/app/routes/usuarios/pages/usuarios-login/usuarios-login.page.ts
+++ frontend/src/app/routes/usuarios/pages/usuarios-login/usuarios-login.page.ts
@@ -1,6 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { IonCard, IonInputPasswordToggle, IonCardSubtitle, IonRouterLinkWithHref } from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonInputPasswordToggle,
+  IonCardSubtitle,
+  IonRouterLinkWithHref,
+} from '@ionic/angular/standalone';
 import { IonInput } from '@ionic/angular/standalone';
 import { Credenciales } from '../../../../shared/types/credenciales';
 import { FormsModule } from '@angular/forms';
@@ -8,29 +13,36 @@ import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-usuarios-login',
-  imports: [IonInput, IonCard, IonInputPasswordToggle, IonCardSubtitle, IonRouterLinkWithHref, RouterLink, FormsModule],
+  imports: [
+    IonInput,
+    IonCard,
+    IonInputPasswordToggle,
+    IonCardSubtitle,
+    IonRouterLinkWithHref,
+    RouterLink,
+    FormsModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './usuarios-login.page.html',
   styleUrl: './usuarios-login.page.css',
 })
 export class UsuariosLoginPage {
-  private authService = inject(AuthService)
-  private router = inject(Router)
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   credenciales = signal<Credenciales>({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
-  async handleSessionInit(event: any){
-    console.log("credenciales: ",this.credenciales())
+  async handleSessionInit(event: any) {
+    console.log('credenciales: ', this.credenciales());
 
     try {
-      await this.authService.logIn(this.credenciales())
-      this.router.navigate(['/home'])
-    }
-    catch (e) {
-      console.log(e)
+      await this.authService.logIn(this.credenciales());
+      this.router.navigate(['/home']);
+    } catch (e) {
+      console.log(e);
     }
   }
 }
--- frontend/src/app/shared/types/usuario.ts
+++ frontend/src/app/shared/types/usuario.ts
@@ -1,13 +1,15 @@
 export type Usuario = {
-    id_usuario: number,
-    email: string,
-    nombres: string,
-    apellidos: string,
-    direccion: string,
-    is_admin: boolean,
-    id_departamento: number,
-    nro_documento: string,
-    foto_url: string,
-    reputacion: number,
-    articulos_carrito: number[]
-}
+  id_usuario: number;
+  email: string;
+  nombres: string;
+  apellidos: string;
+  direccion: string;
+  is_admin: boolean;
+  id_departamento: number;
+  nro_documento: string;
+  foto_url: string;
+  reputacion: number;
+  articulos_carrito: number[];
+};
+
+export type UsuarioSinId = Omit<Usuario, 'id_usuario'>;
