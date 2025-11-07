8d271e1b9bba8f897ba5dad1bba009eddb3867a3
Agustin <agustincigaran@icloud.com>
2025-11-07 20:58:32 -0300

    resenias raro

--- api/src/services/data/reseniasDB.ts
+++ api/src/services/data/reseniasDB.ts
@@ -1,39 +1,67 @@
 import { BasePgRepository } from "../../models/common/baseRepository.ts";
-import { PC_NotFound, PC_BadRequest, PC_InternalServerError, PC_NotImplemented } from "../../errors/errors.ts";
+import {
+  PC_InternalServerError,
+  PC_NotImplemented,
+} from "../../errors/errors.ts";
 import type { Pool } from "pg";
-import type { Categoria } from "../../models/market/categoriaModel.ts";
-
+import { type Resenia } from "../../models/market/reseniaModel.ts";
+import { type ReseniaPost } from "../../models/market/reseniaModel.ts";
 
 // TODO: REALIZAR ESTO ! ! !
-export class CategoriasDB extends BasePgRepository<Categoria> {
+export class ReseniaDB extends BasePgRepository<Resenia> {
+  constructor(pool: Pool) {
+    super(pool);
+  }
 
-    constructor(pool: Pool) {
-        super(pool)
-    }
+  private getQuery(whereCondition: string | null = null) {
+    return /*sql*/ `
+            SELECT
+                r.*
+            FROM resenias r
+            ${whereCondition ?? ""}
+            ;
+        `;
+  }
 
-    #baseQuery = /*sql*/`
-                SELECT 
-                    u.*
-                FROM categorias u
-                `
+  async getAll(): Promise<Resenia[]> {
+    const reseniasTotales = await this.pool.query<Resenia>(this.getQuery());
+    console.log("ojito", reseniasTotales);
+    return reseniasTotales.rows;
+  }
 
-    async getAll(): Promise<Categoria[]> {
-        throw new PC_NotImplemented()
-    }
-    
-    async getById(id:number): Promise<Categoria> {
-        throw new PC_NotImplemented()
-    }
+  async getById(id: number): Promise<Resenia> {
+    throw new PC_NotImplemented();
+  }
 
-    async create(data: Partial<Categoria>): Promise<Categoria> {
-        throw new PC_NotImplemented()
+  async create(data: Partial<ReseniaPost>): Promise<Resenia> {
+    const { id_vendedor, comentario, reputacion } = data;
+    let query = `
+        WITH nueva_resenia AS (
+            insert into resenias ( id_vendedor, comentario, reputacion)
+                VALUES ($1, $2, $3)
+                RETURNING *
+            )
+        SELECT * from nueva_resenia;
+    `;
+    try {
+      const res = await this.pool.query(query, [
+        id_vendedor,
+        comentario,
+        reputacion,
+      ]);
+      return res.rows[0];
+    } catch (e: any) {
+      throw new PC_InternalServerError(
+        "Error en la creacion de una reseña: " + e
+      );
     }
+  }
 
-    async update(id: number, data: Partial<Categoria>): Promise<Categoria> {
-        throw new PC_NotImplemented()
-    }
+  async update(id: number, data: Partial<Resenia>): Promise<Resenia> {
+    throw new PC_NotImplemented();
+  }
 
-    async delete(id: number): Promise<void> {
-        throw new PC_NotImplemented()
-    }
+  async delete(id: number): Promise<void> {
+    throw new PC_NotImplemented();
+  }
 }
