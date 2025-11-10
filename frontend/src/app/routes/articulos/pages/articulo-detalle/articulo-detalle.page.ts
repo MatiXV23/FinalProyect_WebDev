c5c2858b484a5298dfeeebd79b5619f96ff7baca
Agustin <agustincigaran@icloud.com>
2025-11-10 20:40:42 -0300

    detalles articulo v2

--- frontend/src/app/routes/articulos/pages/articulo-detalle/articulo-detalle.page.ts
+++ frontend/src/app/routes/articulos/pages/articulo-detalle/articulo-detalle.page.ts
@@ -6,13 +6,15 @@ import {
   IonCardTitle,
   IonCardSubtitle,
   IonCardContent,
+  IonButton,
+  IonBackButton,
 } from '@ionic/angular/standalone';
 import { ArticulosService } from '../../../../shared/services/articulos.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-articulo-detalle',
-  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent],
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton],
   templateUrl: './articulo-detalle.page.html',
   styleUrl: './articulo-detalle.page.css',
 })
