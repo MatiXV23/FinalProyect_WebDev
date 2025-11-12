608a0a4395a0407da964fd85f4e409e23e86ca65
brahnu <nunezbrahian@gmail.com>
2025-11-12 08:07:46 -0300

    Validators Login

--- frontend/src/app/routes/usuarios/pages/usuarios-login/usuarios-login.page.ts
+++ frontend/src/app/routes/usuarios/pages/usuarios-login/usuarios-login.page.ts
@@ -1,30 +1,36 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, inject, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import {
   IonCard,
-  IonInputPasswordToggle,
   IonCardSubtitle,
-  IonRouterLinkWithHref,
+  IonCardTitle,
+  IonInput,
+  IonInputPasswordToggle,
+  IonText,
+  IonButton,
 } from '@ionic/angular/standalone';
-import { IonInput } from '@ionic/angular/standalone';
-import { Credenciales } from '../../../../shared/types/credenciales';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../../../../shared/services/auth.service';
+import { Credenciales } from '../../../../shared/types/credenciales';
 
 @Component({
   selector: 'app-usuarios-login',
+  templateUrl: './usuarios-login.page.html',
+  styleUrls: ['./usuarios-login.page.css'],
   imports: [
-    IonInput,
-    IonCard,
-    IonInputPasswordToggle,
-    IonCardSubtitle,
-    IonRouterLinkWithHref,
-    RouterLink,
+    CommonModule,
     FormsModule,
+    IonCard,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonInput,
+    IonInputPasswordToggle,
+    IonText,
+    IonButton,
+    RouterLink,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  templateUrl: './usuarios-login.page.html',
-  styleUrl: './usuarios-login.page.css',
 })
 export class UsuariosLoginPage {
   private authService = inject(AuthService);
@@ -35,14 +41,17 @@ export class UsuariosLoginPage {
     password: '',
   });
 
-  async handleSessionInit(event: any) {
-    console.log('credenciales: ', this.credenciales());
+  async handleSessionInit(form: NgForm) {
+    if (form.invalid) {
+      Object.values(form.controls).forEach((c) => c.markAsTouched());
+      return;
+    }
 
     try {
       await this.authService.logIn(this.credenciales());
       this.router.navigate(['/home']);
     } catch (e) {
-      console.log(e);
+      console.error('Error al iniciar sesión:', e);
     }
   }
 }
