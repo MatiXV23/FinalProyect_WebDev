04d75f1b2821dfc7517bdbb12c170a7f21da709f
Agustin <agustincigaran@icloud.com>
2025-11-14 16:15:13 -0300

    refactory front

--- frontend/src/app/routes/chats/pages/mensajes-detalle/mensajes-detalle.page.ts
+++ frontend/src/app/routes/chats/pages/mensajes-detalle/mensajes-detalle.page.ts
@@ -2,7 +2,7 @@ import { Component, effect, inject, input, OnInit, resource } from '@angular/cor
 import { ActivatedRoute } from '@angular/router';
 import { ChatsService } from '../../../../shared/services/chats.service';
 import { MainStore } from '../../../../shared/stores/main.store';
-import { IonInput, IonIcon, IonButton } from "@ionic/angular/standalone";
+import { IonInput, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { WebsocketService } from '../../../../shared/services/websocket.service';
 
@@ -12,43 +12,42 @@ import { WebsocketService } from '../../../../shared/services/websocket.service'
   templateUrl: './mensajes-detalle.page.html',
   styleUrl: './mensajes-detalle.page.css',
 })
-export class MensajesDetallePage{
-  
-  private wsService = inject(WebsocketService)
+export class MensajesDetallePage {
+  private wsService = inject(WebsocketService);
   private route = inject(ActivatedRoute);
-  private chatService = inject(ChatsService)
-  private mainStore = inject(MainStore)
+  private chatService = inject(ChatsService);
+  private mainStore = inject(MainStore);
 
   id_chat = this.route.snapshot.paramMap.get('id_chat');
 
-  fecha_vieja = '1/1/2001'
+  fecha_vieja = '1/1/2001';
 
-  contenido = ''
+  contenido = '';
 
   mensajes = resource({
-    params: () => ({id: this.id_chat}),
-    loader: ({params}) => this.chatService.getMensajes(params.id!)
-  })
+    params: () => ({ id: this.id_chat }),
+    loader: ({ params }) => this.chatService.getMensajes(params.id!),
+  });
 
-  msgScroll = effect(()=> {
-    const msgs = this.mensajes.value()
+  msgReload = effect(() => {
+    if (this.wsService.shouldMsgReload()) {
+      this.mensajes.reload();
+      this.wsService.resetReload();
+    }
+  });
+
+  msgScroll = effect(() => {
+    const msgs = this.mensajes.value();
     if (msgs) {
-      this.scrollDown()
+      this.scrollDown();
     }
-  })
+  });
 
-  msgReload = effect(()=> {
-    if (this.wsService.shouldReload()) {
-      this.mensajes.reload()
-      this.wsService.resetReload()
-    }
-  })
-  
   isUserLogged(id_usuario: number): boolean {
-    return this.mainStore.isUserLogged(id_usuario)
+    return this.mainStore.isUserLogged(id_usuario);
   }
 
-  parseDate(dateTime: string){
+  parseDate(dateTime: string) {
     const date = new Date(dateTime);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -57,38 +56,36 @@ export class MensajesDetallePage{
     return `${day}/${month}/${year}`;
   }
 
-  parseTime(dateTime: string){ 
+  parseTime(dateTime: string) {
     const date = new Date(dateTime);
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    
+
     return `${hours}:${minutes}`;
   }
 
-  debeImprimirFecha(fecha_actual: string, fecha_vieja:string): boolean{
-    return fecha_vieja !== fecha_actual
+  debeImprimirFecha(fecha_actual: string, fecha_vieja: string): boolean {
+    return fecha_vieja !== fecha_actual;
   }
 
-  async sendMessage(event: any){
-    if (!this.contenido) return
-    await this.chatService.sendMessage(this.id_chat!, this.contenido)
+  async sendMessage(event: any) {
+    if (!this.contenido) return;
+    await this.chatService.sendMessage(this.id_chat!, this.contenido);
     setTimeout(() => {
-      this.contenido = ''
-    }, 0)
-    this.mensajes.reload()
+      this.contenido = '';
+    }, 0);
+    this.mensajes.reload();
   }
 
   scrollDown() {
     setTimeout(() => {
-      const element = document.getElementById('bottomCont')
+      const element = document.getElementById('bottomCont');
       if (element) {
         element.scrollIntoView({
           behavior: 'smooth',
           block: 'start',
-        })
+        });
       }
     });
   }
 }
-
-
