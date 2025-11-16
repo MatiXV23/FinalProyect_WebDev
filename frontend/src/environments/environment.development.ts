1720149247bbb4d984508bb17a4c3001acfb4e8f
Mati <mperezfrontan@gmail.com>
2025-11-16 07:07:21 -0300

    small fix

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl : "http://192.168.1.38:3000",
-    wsUrl : "ws://192.168.1.38:3000/ws?id_usuario="
+    apiUrl : "http://localhost:3000",
+    wsUrl : "ws://localhost:3000/ws?id_usuario="
 };
