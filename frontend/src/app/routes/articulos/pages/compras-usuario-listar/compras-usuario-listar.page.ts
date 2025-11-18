0ed1c85bf266d4dbbd35e44320540448a962a1ce
Agustin <agustincigaran@icloud.com>
2025-11-18 15:36:31 -0300

    listado compras

--- frontend/src/app/routes/articulos/pages/compras-usuario-listar/compras-usuario-listar.page.ts
+++ frontend/src/app/routes/articulos/pages/compras-usuario-listar/compras-usuario-listar.page.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource } from '@angular/core';
+import { UsuariosService } from '../../../../shared/services/usuarios.service';
+import { MainStore } from '../../../../shared/stores/main.store';
+import {
+  IonSelect,
+  IonCard,
+  IonCardTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-compras-usuario-listar',
-  imports: [],
+  imports: [IonCard, IonCardTitle, IonGrid, IonRow, IonCol],
   templateUrl: './compras-usuario-listar.page.html',
   styleUrl: './compras-usuario-listar.page.css',
 })
 export class ComprasUsuarioListarPage {
-
+  private usuarioService = inject(UsuariosService);
+  private mainStore = inject(MainStore);
+  public compras = resource({
+    loader: () => this.usuarioService.getComprasByIdUser(Number(this.mainStore.user()?.id_usuario)),
+  });
 }
