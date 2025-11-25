d6ad7b13d7fda5dd81db965a9d6974e13b6cc8d9
Mati <mperezfrontan@gmail.com>
2025-11-25 20:34:18 -0300

    verificaciones de maxLength

--- frontend/src/app/shared/validators/documento-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentoValidatorDirective } from './documento-validator.directive';
-
-describe('DocumentoValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DocumentoValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
